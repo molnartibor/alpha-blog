70955b0e84d1ae5da8dcf91b0e7636652b1c17cf
dibi <molnar_tibor@centrum.sk>
2016-11-16 12:06:36 +0000

    Create show action and add gravatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,8 +25,12 @@ class UsersController < ApplicationController
        else
         render 'edit'
        end
-       
    end
+    
+    def show
+        @user = User.find(params[:id])
+        
+    end
    
    private
    def user_params
